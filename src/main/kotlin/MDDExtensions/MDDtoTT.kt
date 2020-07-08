b49d8d1ef5d5b6d21207eadc9130b585fa659ea8
Daniel <szekeresdani@gmail.com>
2020-07-08 21:10:35 +0200

    fix: domain size of the variable is used now for mode size

--- src/main/kotlin/MDDExtensions/MDDtoTT.kt
+++ src/main/kotlin/MDDExtensions/MDDtoTT.kt
@@ -8,7 +8,7 @@ fun MddHandle.toTensorTrain(): TensorTrain {
     val cores = arrayListOf<CoreTensor>()
 
     var nextNodes = Array(this.size()) {this[it]}.toSet().toList()
-    val firstCore = CoreTensor(this.size(), 1, nextNodes.size)
+    val firstCore = CoreTensor(this.variableHandle.variable.get().domainSize, 1, nextNodes.size)
     for (i in 0 until this.size()) {
         firstCore[i][nextNodes.indexOf(this[i])] = 1.0
     }
@@ -18,7 +18,7 @@ fun MddHandle.toTensorTrain(): TensorTrain {
         val currNodes = nextNodes
         nextNodes = currNodes.flatMap { node -> Array(node.size()) { node[it] }.toList() }.toSet().toList()
         if(nextNodes[0].isTerminal) nextNodes = nextNodes.filter { it.data == true }
-        val newCore = CoreTensor(currNodes[0].size(), currNodes.size, nextNodes.size)
+        val newCore = CoreTensor(currNodes[0].variableHandle.variable.get().domainSize, currNodes.size, nextNodes.size)
         for ((idx, currNode) in currNodes.withIndex()) {
             for (edge in 0 until currNode.size()) {
                 val targetIdx = nextNodes.indexOf(currNode[edge])
